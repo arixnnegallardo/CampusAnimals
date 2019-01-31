51a3026341f586a65cc25d91824c07ddab2633da
U-DESKTOP-V22CBSO\arianne <a.rianne13@yahoo.com>
2019-02-01 06:40:28 +0800

    first

---
+++ app/controllers/animalprofile_controller.rb
@@ -0,0 +1,5 @@
+class AnimalprofileController < ApplicationController
+	def add
+		@okay = 1;
+	end
+end
---
+++ test/controllers/animalprofile_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnimalprofileControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
